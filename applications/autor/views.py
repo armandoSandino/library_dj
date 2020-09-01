7049fb04500e63d47064816f0372f12aedd9c957
Juan Sandino <jsandino@aimservices.tech>
2020-09-01 10:24:05 -0600

    feat: add generic view list of authors

--- applications/autor/views.py
+++ applications/autor/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+# Generic views
+from django.views.generic import  ListView
 
-# Create your views here.
+# Models
+from .models import Autor
+
+class ListAutores(ListView):
+
+    template_name = 'autor/lista.html'
+    # model = Autor
+    context_object_name = 'listAutores'
+    success_url = '/'
+
+    
+    def get_context_data(self, **kwargs):
+        context = super(ListAutores, self).get_context_data(**kwargs)
+        context['titutlo'] = 'Los autores'
+        return context
+    
+    def get_queryset(self):
+        # queryset = super(ListAutores, self).get_queryset()
+        # queryset = queryset
+        return Autor.objects.all()
+
+    
