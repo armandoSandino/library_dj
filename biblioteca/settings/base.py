629cbaeaa73d047fcfe193619c1bb0d65fd35624
Juan Sandino <jsandino@aimservices.tech>
2020-08-31 18:07:58 -0600

    chore:  add applications to the 'base.py' configuration file

--- biblioteca/settings/base.py
+++ biblioteca/settings/base.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # apps
+    'applications.autor',
+    'applications.lector',
+    'applications.libro'
 ]
 
 MIDDLEWARE = [
