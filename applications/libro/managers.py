fe9b2b4a3f050e51d06feeae269f42f909891a56
Juan Sandino <jsandino@aimservices.tech>
2020-09-01 15:17:16 -0600

    feat: search book by date range

--- applications/libro/managers.py
+++ applications/libro/managers.py
@@ -11,7 +11,9 @@ class LibroManager(models.Manager):
     # Busca un libro  
     def buscar_libro(self, termino):
         # Recurde que el __icontains busca conincidencias( like ) en un campo
+        # __range, busca registros que este en un rango de fecha, el 1er argumento es la fecha incial el 2do la final
         resultado = self.filter(
-            titulo__icontains= termino
+            titulo__icontains= termino,
+            fecha__range=('2020-01-01', '2020-08-01')
         )
         return resultado
