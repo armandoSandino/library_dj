8865f7be6f08198f9a0dce4bfbe801f8516a1337
Juan Sandino <jsandino@aimservices.tech>
2020-09-02 11:48:07 -0600

    chore: agregar el modelo Persona al Administrador

--- applications/home/admin.py
+++ applications/home/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Persona
 
-# Register your models here.
+class PersonaAdmin(admin.ModelAdmin):
+
+    # Campos a mostrar
+    list_display = (
+        'id',
+        'full_name',
+        'pais',
+        'pasaporte',
+        'edad',
+        'apelativo'
+    )
+    search_fields = (
+        'full_name',
+        'edad'
+    )
+    # Habilitar filtros laterales en el administrador
+    list_filter = (
+        'pais',
+        'edad'
+    )
+
+admin.site.register(Persona, PersonaAdmin)
